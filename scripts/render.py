c23ae631742d6a74946f66ba30308b2be686cb67
Quinn McCaffrey <quinn@versaliminal.org>
2025-12-30 11:12:02 -0600

    Cleanup and commenting

--- scripts/render.py
+++ scripts/render.py
@@ -26,6 +26,9 @@ INCLUDE_FMT = '\\input{{rendered/{output}}} \\clearpage\n'
 YAML_TAG = '(yaml)'
 
 def read_conifg(content_root, project):
+    """
+    Reads the project config file.
+    """
     config_file = CONFIG_FILE_FMT.format(content_root=content_root)
     print("Loading projects file: {0}".format(config_file))
     with open(config_file, 'r') as file:
@@ -34,6 +37,9 @@ def read_conifg(content_root, project):
             return config[project]
 
 def refresh_sources(project_dir, config):
+    """
+    Refreshes local tables from sheets in Google Sheets.
+    """
     try:
         os.mkdir(project_dir)
         os.mkdir(TABLE_PATH_FMT.format(""))
@@ -52,23 +58,35 @@ def refresh_sources(project_dir, config):
             print("\t- Error downloading file:", e)
 
 def clear_rendered(project_dir):
+    """
+    Destroys and re-creates the rendered directory.
+    """
     rendered_dir = RENDERED_PATH_FMT.format(project_dir=project_dir, output="")
     if os.path.exists(rendered_dir):
         shutil.rmtree(rendered_dir)
     os.mkdir(rendered_dir)
 
 def jinja_to_latex_arg(content):
+    """
+    Formats a single argument as a latex argument (string, int, or float only).
+    """
     if content:
-        if isinstance(content, int):
+        if isinstance(content, int) or isinstance(content, float):
             content = str(content)
         if isinstance(content, str):
             return '{' + content + '}'
     return '{}'
 
 def jinja_to_latex_args(*args):
+    """
+    Formats any number of arguments as latex arguments.
+    """
     return "".join(map(jinja_to_latex_arg, args))
 
 def render_templates(project_dir, config, last_run):
+    """
+    Runs all table and template mappings.
+    """
     tables_dir = TABLE_PATH_FMT.format(project_dir=project_dir, table="")
     templates_dir = TEMPLATE_PATH_FMT.format(project_dir=project_dir, template="")
 
@@ -85,6 +103,9 @@ def render_templates(project_dir, config, last_run):
         render_template(project_dir, table_file_path, templates_dir, entry['template'])
 
 def render_template(project_dir, table_file_path, template_dir, template_name):
+    """
+    Runs the specified template for every row in the specified table.
+    """
     includes_file_path = RENDERED_PATH_FMT.format(project_dir=project_dir, output=INCLUDES_FILE)
     includes_file = open(includes_file_path, 'a')
     with open(table_file_path, 'r', newline='') as table_file:
@@ -111,21 +132,53 @@ def render_template(project_dir, table_file_path, template_dir, template_name):
                 tex_file.write(template.render(raw=row, parsed=parsed_cols))
         includes_file.close()
 
-def get_and_update_last_run():
+def run_latex(project_dir, project, config, look):
+    """
+    Runs pdflatex with configured inputs and optionally shows output for the project.
+    """
+    try:
+        os.mkdir(PDF_DIR_FMT.format(project_dir=project_dir))
+    except FileExistsError:
+        pass
+    latex_args = ['pdflatex', '-interaction=nonstopmode', '-output-directory=pdf']
+    result = subprocess.run(latex_args + config['includes'], cwd=project_dir, capture_output=True, text=True)
+    if result.returncode != 0:
+        print("  ! Completed with status {0}, check logs".format(result.returncode))
+    else:
+        print('  * completed successfully')
+    
+    if look:
+        pdf_path = PDF_PATH_FMT.format(project_dir=project_dir, project=project)
+        subprocess.run(['qlmanage', '-p', pdf_path])
+
+def get_last_run(project_dir):
+    """
+    Gets the mtime of the last .lastrun file or 0.0 if it does not exist
+    """
     last_run_file = LASTRUN_FILE_FMT.format(project_dir=project_dir)
     try:
-        mtime = os.path.getmtime(last_run_file)
+        return os.path.getmtime(last_run_file)
     except FileNotFoundError:
-        mtime = 0
+        return 0.0
+
+def update_last_run(project_dir):
+    """
+    Updates the mtime of the .lastrun file for the project, creating it if it doesn't exist.
+    """
+    last_run_file = LASTRUN_FILE_FMT.format(project_dir=project_dir)
     with open(last_run_file, 'w') as lastrun:
         lastrun.truncate(0)    
-    return mtime
 
-if __name__ == '__main__':
+
+def main():
+    """
+    Implements main workflow.
+    """
     parser = argparse.ArgumentParser(description='A tool for refreshing CSV sources from google sheets')
     parser.add_argument('project', help='Project name')
     parser.add_argument('--root', help='Root content directory', default=os.getcwd())
     parser.add_argument('--refresh', help='Refresh upstream sources', action='store_true')
+    parser.add_argument('--force', help='Skip all precondition checks', action='store_true')
     parser.add_argument('--look', help='Open oututs with quicklook', action='store_true')
     args = parser.parse_args()
     print("Rendering: root={0}, project={1}".format(args.root, args.project))
@@ -140,29 +193,17 @@ if __name__ == '__main__':
         refresh_sources(project_dir, config)
     
     print("Rendering templates...")
-    last_run = get_and_update_last_run()
+    last_run = get_last_run(project_dir)
+    if args.force:
+        last_run = 0.0
     render_templates(project_dir, config, last_run)
 
     outputs = config['outputs']
     if outputs['latex']:
         print("Running LaTex...")
-        try:
-            os.mkdir(PDF_DIR_FMT.format(project_dir=project_dir))
-        except FileExistsError:
-            pass
-        latex_args = ['pdflatex', '-interaction=nonstopmode', '-output-directory=pdf']
-        result = subprocess.run(latex_args + outputs['latex']['includes'], cwd=project_dir, capture_output=True, text=True)
-        if result.returncode != 0:
-            print("  ! Completed with status {0}, check logs".format(result.returncode))
-        else:
-            print('  * completed successfully')
-        
-        if args.look:
-            pdf_path = PDF_PATH_FMT.format(project_dir=project_dir, project=args.project)
-            subprocess.run(['qlmanage', '-p', pdf_path])
+        run_latex(project_dir, args.project, outputs['latex'], args.look)
 
-    
+    update_last_run(project_dir)
 
-    
-    
-
+if __name__ == '__main__':
+    main()
