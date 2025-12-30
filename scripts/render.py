5a7a1532d7b5d39fb16d189b6d3d46ecbc7d222f
Quinn McCaffrey <quinn@versaliminal.org>
2025-12-30 10:51:11 -0600

    Check mtimes before templating

--- scripts/render.py
+++ scripts/render.py
@@ -1,6 +1,7 @@
 #!python
 import argparse
 import csv
+import time
 import jinja2
 import os
 import shutil
@@ -11,6 +12,7 @@ import yaml
 GSHEETS_URL_FMT = '{url}/gviz/tq?tqx=out:csv&sheet={sheet}'
 
 PROJECT_DIR_FMT = '{root}/{project}'
+LASTRUN_FILE_FMT = '{project_dir}/.lastrun'
 TABLE_PATH_FMT = '{project_dir}/tables/{table}'
 TEMPLATE_PATH_FMT = '{project_dir}/templates/{template}'
 RENDERED_PATH_FMT = '{project_dir}/rendered/{output}'
@@ -19,7 +21,7 @@ INCLUDES_FILE = 'includes.tex'
 CONFIG_FILE_FMT = '{content_root}/projects.yaml'
 PDF_PATH_FMT = '{project_dir}/pdf/{project}.pdf'
 
-INCLUDE_FMT = '\\input{{rendered/{output}}} \clearpage\n'
+INCLUDE_FMT = '\\input{{rendered/{output}}} \\clearpage\n'
 
 YAML_TAG = '(yaml)'
 
@@ -66,19 +68,28 @@ def jinja_to_latex_arg(content):
 def jinja_to_latex_args(*args):
     return "".join(map(jinja_to_latex_arg, args))
 
-def render_templates(project_dir, config):
-    for entry in config['mappings']:
-        render_template(project_dir, entry['table'], entry['template'])
+def render_templates(project_dir, config, last_run):
+    tables_dir = TABLE_PATH_FMT.format(project_dir=project_dir, table="")
+    templates_dir = TEMPLATE_PATH_FMT.format(project_dir=project_dir, template="")
 
-def render_template(project_dir, table_name, template_name):
+    mtime = max(max(os.path.getmtime(root) for root,_,_ in os.walk(tables_dir)),
+                max(os.path.getmtime(root) for root,_,_ in os.walk(tables_dir)))
+    if last_run > mtime:
+        print ("  ! No updates to tables or templates found, skipping templating: last-run={0}, last-update={1}"
+               .format(time.ctime(last_run), time.ctime(mtime)))
+        return
+
+    clear_rendered(project_dir)
+    for entry in config['mappings']:
+        table_file_path = TABLE_PATH_FMT.format(project_dir=project_dir, table=entry['table'])
+        render_template(project_dir, table_file_path, templates_dir, entry['template'])
+
+def render_template(project_dir, table_file_path, template_dir, template_name):
     includes_file_path = RENDERED_PATH_FMT.format(project_dir=project_dir, output=INCLUDES_FILE)
     includes_file = open(includes_file_path, 'a')
-
-    table_file_path = TABLE_PATH_FMT.format(project_dir=project_dir, table=table_name)
     with open(table_file_path, 'r', newline='') as table_file:
         reader = csv.DictReader(table_file)
 
-        template_dir = TEMPLATE_PATH_FMT.format(project_dir=project_dir, template="")
         env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
         env.globals.update(arg=jinja_to_latex_arg, args=jinja_to_latex_args)
         template = env.get_template(template_name)
@@ -100,6 +111,16 @@ def render_template(project_dir, table_name, template_name):
                 tex_file.write(template.render(raw=row, parsed=parsed_cols))
         includes_file.close()
 
+def get_and_update_last_run():
+    last_run_file = LASTRUN_FILE_FMT.format(project_dir=project_dir)
+    try:
+        mtime = os.path.getmtime(last_run_file)
+    except FileNotFoundError:
+        mtime = 0
+    with open(last_run_file, 'w') as lastrun:
+        lastrun.truncate(0)    
+    return mtime
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='A tool for refreshing CSV sources from google sheets')
     parser.add_argument('project', help='Project name')
@@ -118,9 +139,9 @@ if __name__ == '__main__':
         print("Refreshing sources...")
         refresh_sources(project_dir, config)
     
-    clear_rendered(project_dir)
     print("Rendering templates...")
-    render_templates(project_dir, config)
+    last_run = get_and_update_last_run()
+    render_templates(project_dir, config, last_run)
 
     outputs = config['outputs']
     if outputs['latex']:
